120bbc0cf9a89e0465072fbd3f081213f53fbd2b
Ravindra Benadict Asirvatham <ravindra_github@bluewin.ch>
2022-12-26 18:49:53 +0100

    Example of sharing data between parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-blog-app';
+  parentMessage:string = 'Message coming from parent component'
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  title: string = "List of Posts"
+  
+  @Input() fromParent!: string;
 
+  constructor () {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
